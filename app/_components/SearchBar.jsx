6e27e318dc87a72d9bd9e3c5db512ed792e40cd0
Gamze <gamzeyasar35@icloud.com>
2023-11-20 11:14:30 +0300

    search

--- app/_components/SearchBar.jsx
+++ app/_components/SearchBar.jsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 export default function Page() {
   const [search, setSearch] = useState();
   return (
-    <input
-      type="text"
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-    />
+    <>
+      <p>{search}</p>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </>
   );
 }
